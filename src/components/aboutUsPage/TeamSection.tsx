e9c2306317f36f8cb7f459c9b13e07788945aa71
Michael Gitonga <gitoshmbae@gmail.com>
2025-09-16 10:55:25 +0300

    Adds a new team members data in src/constData/aboutPage.ts, Displays the new team members in src/components/aboutUsPage/TeamSection.tsx

--- src/components/aboutUsPage/TeamSection.tsx
+++ src/components/aboutUsPage/TeamSection.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import CloudinaryImage from "../CloudinaryImage";
 import { Facebook, Instagram, Linkedin, X } from "lucide-react";
+import { teamMembers } from "@/constData/aboutPage";
 
 export default function TeamSection() {
   return (
@@ -18,32 +19,79 @@ export default function TeamSection() {
             />
           </div>
           <div className="w-full md:w-6/12 lg:w-4/12">
-            <h2 className="text-2xl font-thin text-stone-400 mt-12 mb-12">The Team</h2>
+            <h2 className="text-2xl font-thin text-stone-400 mt-12 mb-12">
+              The Team
+            </h2>
             <div className="space-y-4">
-                <h3 className="text-4xl font-bold 0">Joanne Ndirangu</h3>
-                <p className="text2xl font-bold">Founder & Director</p>
-                <p className=" leading-8">
-                &quot;Supporting accessible tourism has been incredibly rewarding.
-                Knowing that my contribution helps create inclusive travel
-                experiences for all makes a real difference.&quot;
-                </p>
-                <div className="w-32 flex  justify-evenly items-center gap-2">
-                <Link href={""}>
-                    <X  className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1"/>
+              <h3 className="text-4xl font-bold 0">Joanne Ndirangu</h3>
+              <p className="text2xl font-bold">Founder & Director</p>
+              <p className=" leading-8">
+                &quot;Supporting accessible tourism has been incredibly
+                rewarding. Knowing that my contribution helps create inclusive
+                travel experiences for all makes a real difference.&quot;
+              </p>
+              <div className="w-32 flex  justify-evenly items-center gap-2">
+                <Link href={"https://x.com/home"}>
+                  <X className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1" />
                 </Link>
-                <Link href={""}>
-                    <Instagram className=" size-7 hover:scale-110 transition duration-300 rounded-lg"/>
+                <Link href={"https://www.instagram.com/"}>
+                  <Instagram className=" size-7 hover:scale-110 transition duration-300 rounded-lg" />
                 </Link>
-                <Link href={""}>
-                    <Facebook className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1" />
+                <Link href={"https://www.facebook.com/"}>
+                  <Facebook className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1" />
                 </Link>
-                <Link href={""}>
-                    <Linkedin className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1" />
+                <Link href={"https://www.linkedin.com/"}>
+                  <Linkedin className="border border-black size-7 hover:scale-110 transition duration-300 rounded-lg p-1" />
                 </Link>
-                </div>
-
+              </div>
             </div>
           </div>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-12 mt-20 ">
+          {teamMembers.map((member) => (
+            <div
+              key={member.id}
+              className="flex flex-wrap md:flex-nowrap justify-center items-center gap-12"
+            >
+              <div className="w-full md:w-6/12 lg:w-12/12">
+                <div className="min-h-80 w-11/12 rounded-lg bg-scoutGreen4"></div>
+                <CloudinaryImage
+                  src={member.imageSrc}
+                  height={800}
+                  width={800}
+                  alt={`${member.name} image`}
+                  // className="object-cover h-full w-full rounded-lg -mt-80 pl-6"
+                  className="object-top object-cover  h-80 w-full rounded-lg -mt-64 pl-6"
+                />
+              </div>
+              <div className="w-full md:w-6/12 lg:w-6/12">
+                <h2 className="hidden text-2xl font-thin text-stone-400 mt-12 mb-12">
+                  The Team
+                </h2>
+                <div className="space-y-4">
+                  <h3 className="text-2xl font-bold 0">{member.name}</h3>
+                  <p className="text-">{member.position}</p>
+                  <p className=" hidden leading-8">
+                    &quot;{member.statement}&quot;
+                  </p>
+                  <div className="w-32 flex  justify-evenly items-center gap-2">
+                    <Link href={member.xLink}>
+                      <X className="border border-black size-5 hover:scale-110 transition duration-300 rounded-lg p-1" />
+                    </Link>
+                    <Link href={member.instagramLink}>
+                      <Instagram className=" size-5 hover:scale-110 transition duration-300 rounded-lg" />
+                    </Link>
+                    <Link href={member.facebookLink}>
+                      <Facebook className="border border-black size-5 hover:scale-110 transition duration-300 rounded-lg p-1" />
+                    </Link>
+                    <Link href={member.linkedInLink}>
+                      <Linkedin className="border border-black size-5 hover:scale-110 transition duration-300 rounded-lg p-1" />
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
---
+++ src/constData/aboutPage.ts
@@ -0,0 +1,38 @@
+export const teamMembers=[
+    {
+        id:1,
+        name:"Nimi Natalie",
+        imageSrc:"Nimi_PhotoGrid_s47f8v",
+        position:"Advisory Board Member - Data Analytics",
+        statement:"",
+        linkedInLink:"https://www.linkedin.com/",
+        xLink:"https://x.com/home",
+        instagramLink:"https://www.instagram.com/",
+        facebookLink:"https://www.facebook.com/",
+        
+    },
+    {
+        id:2,
+        name:"Julius Mbura",
+        imageSrc:"Julius_Mbura_renano",
+        position:"Advisory Board Member - Disability Advocate",
+        statement:"",
+        linkedInLink:"https://www.linkedin.com/",
+        xLink:"https://x.com/home",
+        instagramLink:"https://www.instagram.com/",
+        facebookLink:"https://www.facebook.com/",
+        
+    },
+    {
+        id:3,
+        name:"Myles Mugambi",
+        imageSrc:"Myles_Mugambi_PhotoGrid_luyhxo",
+        position:"Fundraising",
+        statement:"",
+        linkedInLink:"https://www.linkedin.com/",
+        xLink:"https://x.com/home",
+        instagramLink:"https://www.instagram.com/",
+        facebookLink:"https://www.facebook.com/",
+        
+    },
+]
